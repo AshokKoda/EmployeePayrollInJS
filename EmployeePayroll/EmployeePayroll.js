6b926aafdea7fdc1cf47d218b4301a9b9832273b
Ashok Koda <a.koda59@gmail.com>
2022-01-20 11:45:16 +0530

    Added:Employee Payroll extended class

--- EmployeePayroll/EmployeePayroll.js
+++ EmployeePayroll/EmployeePayroll.js
@@ -2,12 +2,16 @@ class EmployeePayroll {
 
     id;
     salary;
+    gender;
+    startDate;
 
     //constructor
-    constructor(id, name, salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     //getter and setter method
@@ -18,12 +22,25 @@ class EmployeePayroll {
         this._name = name;
     }
 
+    get startDate() {
+        return this._startDate;
+    }
+    set startDate(startDate) {
+        this._startDate = startDate;
+    }
+
     //method
     toString() {
-        return "id =" + this.id + ", name = " + this.name + ", salary = " + this.salary;
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = this.startDate === undefined ? "undefined" :
+            this.startDate.toLocaleDateString("en-US", options);
+        return "id =" + this.id + ", name = " + this.name + ", salary = " + this.salary + ", gender = " + this.gender + ", startDate = " + empDate;
     }
 }
 let employeePayroll = new EmployeePayroll(1, "Ashok", 20000);
 console.log(employeePayroll);
 employeePayroll.name = "Koda Ashok";
 console.log(employeePayroll.toString());
+
+let newEmployeePayroll = new EmployeePayroll(2, "Terissa", 400000, "F", new Date());
+console.log(newEmployeePayroll.toString()); 
